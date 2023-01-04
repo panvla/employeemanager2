b526303124e8e074c799292e0e2ac862c58a3bf6
panvla <pandvlado@gmail.com>
2023-01-04 19:18:13 +0100

    added initial project.

---
+++ employeemanager2/src/main/java/com/vladimirpandurov/employeemanager2/Employeemanager2Application.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.employeemanager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employeemanager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employeemanager2Application.class, args);
+	}
+
+}
